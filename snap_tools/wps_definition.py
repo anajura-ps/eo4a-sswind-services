349635798281e3ed3492b28819ca588d76737c74
Ana Juracic <ana.juracic@parameterspace.ie>
2017-06-23 10:44:12 +0100

    Added the snap tools repo, for testing

---
+++ snap_tools/wps_definition.py
@@ -0,0 +1,13 @@
+"""
+WPS processes for existing EO tools
+"""
+
+#from gdaltools.gdalinfo import GdalInfo
+#from gdaltools.gdalwarp import GdalWarp
+from tools.cal import Cal
+
+__author__ = "Derek O'Callaghan"
+
+        
+        
+        
